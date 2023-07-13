14dd77dff3d2ef4ae3b5bfc63bffca9749c488d6
joshua Oyewole <joshpp2013@gmail.com>
2023-07-13 12:41:52 +0100

    add use context for global states

---
+++ client/src/context/userContext.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+ const UserContext = React.createContext({
+    firstname: "Joshua",
+    lastname: "Oyewole",
+    profilePixs: "https://comforting-truffle-bb451c.netlify.app/assets/profilePixs.e9b1dd09.png"
+ });
+
+ export default UserContext;
