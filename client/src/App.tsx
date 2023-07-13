b5493d49367cb82a8cfc6e983e08dcb834cd63cf
joshua Oyewole <joshpp2013@gmail.com>
2023-07-13 17:14:16 +0100

    refactored codebase

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,34 +6,30 @@ import 'react-toastify/dist/ReactToastify.css';
 import Register from "./pages/register/index";
 import ResetPassword from "./pages/resetPassword/index";
 import ResetPwdLink from "./pages/resetPassword/resetPwd";
-import UserProfile from "./pages/users/index";
 import Error from "./pages/404/index";
+import ProtectedRoute from "./util/ProtectedRoute";
+
+
 
 function App() {
 
+
   return (
     <>
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
-      {/* Same as */}
       <ToastContainer />
       <Routes>
-        <Route path="/" element={<Homepage />} />
+
+        <Route path="/" element={
+          <ProtectedRoute>
+            <Homepage />
+          </ProtectedRoute>
+        }
+        />
+
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/reset-pwdLink" element={<ResetPwdLink />} />
-        <Route path="/users/:id" element={<UserProfile />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </>
