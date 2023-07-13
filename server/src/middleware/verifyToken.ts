8f3bdc9c5cb286fe629169a22e35ab1e7f2f3163
joshua Oyewole <joshpp2013@gmail.com>
2023-07-13 17:12:20 +0100

    Refactored codebase

---
+++ server/src/middleware/verifyToken.ts
@@ -0,0 +1,38 @@
+import jwt from "jsonwebtoken";
+import createError from "../util/error";
+import { Request, Response, NextFunction } from 'express';
+
+const verifyToken = (req: Request, res: Response, next: NextFunction) => {
+    //Get the User Token
+    let token = req.headers?.authorization?.replace('Bearer ', '');
+    //Verify the User Token
+    if (token) {
+
+        //verify if a token is valid. If not valid throw an error
+        jwt.verify(token, process.env.JWT_SECRET, (err, data) => {
+
+            //if valid access the data in the token in a req variable
+            if (!err) {
+                req.user = data;
+                return next();
+            } else {
+                return createError(403, "Access denied, Incorrect Authorization Code"
+                );
+            }
+        });
+    } else {
+        return next(createError(401, "Access denied, No Authorization code"));
+    }
+};
+
+const verifyUser = (req: Request, res: Response, next: NextFunction) => {
+    verifyToken(req, res, () => {
+        if (req.user.id === req.params.id) {
+            next();
+        } else {
+            return next(createError(403, "You are not authorized!"));
+        }
+    });
+};
+
+export default { verifyToken, verifyUser }
