18cf4d65edd8cdb22969d2e8472b75542693a8a5
joshua Oyewole <joshpp2013@gmail.com>
2023-07-18 20:36:55 +0100

    modified useContext

--- client/src/context/userContext.tsx
+++ client/src/context/userContext.tsx
@@ -1,13 +1,18 @@
 import React from "react"
-import { userType } from "../util/types";
+//import { userType } from "../util/types";
 
- const userInfo:userType = JSON.parse(localStorage.getItem('user')!)
- 
+//const userInfo: userType = JSON.parse(localStorage.getItem('user')!)
+const firstname: string | null = localStorage.getItem('firstname')
+const lastname: string | null = localStorage.getItem('lastname')
+const profilePicture: string | null = localStorage.getItem('profilePicture');
+const user_id: string | null = localStorage.getItem('user_id')
+
+
 const UserContext = React.createContext({
-   firstname: userInfo?.user.firstname,
-   lastname: userInfo?.user.lastname,
-   profilePixs: userInfo?.user.profilePicture ? userInfo?.user.profilePicture : "https://cdn3.iconfinder.com/data/icons/network-communication-vol-3-1/48/111-512.png"
+    firstname,
+    lastname,
+    user_id:user_id,
+    profilePicture: profilePicture ? profilePicture : "https://cdn3.iconfinder.com/data/icons/network-communication-vol-3-1/48/111-512.png"
 });
 
 export default UserContext;
-
