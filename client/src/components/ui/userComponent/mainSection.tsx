9e4102649c280a37f32eaa176214d46ea5cda4fe
joshua Oyewole <joshpp2013@gmail.com>
2023-07-18 20:32:11 +0100

    removed post desc toggle function

--- client/src/components/ui/userComponent/mainSection.tsx
+++ client/src/components/ui/userComponent/mainSection.tsx
@@ -1,37 +1,94 @@
+import InfiniteScroll from 'react-infinite-scroll-component';
 import PostStyles from "../../../components/ui/createPost/_createpost.module.scss"
-import CreatePost from "../createPost/createPost"
-import Post from "../post/post"
+//import Post from "../post/post"
 import Modal from "../createPost/Modal"
 import { postType } from "../../../util/types"
+import { useState, useEffect } from "react"
+import axios from "axios"
+import dayjs from 'dayjs';
+import relativeTime from "dayjs/plugin/relativeTime";
+import { getUserDatas } from '../../../util/auth';
 
 
-type mainSectionType = {
-  posts:postType[]
-}
+const MainSection = () => {
+  const [posts, setPost] = useState<Array<postType>>([]);
+  const [page, setPage] = useState(1);
+  dayjs.extend(relativeTime);
 
-const MainSection = ({posts}:mainSectionType) => {
+  let { token, user_id } = getUserDatas()
 
+  const fetchPost = async () => {
+    try {
+      const response = await axios.get(`http://localhost:5000/post/`,
+        {
+          params: {
+            user_id: `${user_id}`,
+            page,
+            limit: 10
+          },
+          headers: {
+            Authorization: `Bearer ${token}`
+          },
+        });
+
+      setPost((prev: postType[]) => {
+        return [...prev, ...response.data.data];
+      });
+    } catch (error) {
+      throw error
+    }
+  }
+
+  useEffect(() => {
+    fetchPost();
+  }, [page])
 
   return (
     <>
       <Modal />
-      <div className={PostStyles.post__create_postContainer}>
-        <CreatePost />
-      </div>
       <div className={PostStyles.post__posts_container}>
-         {
-          posts.map((post, index) => {
-            return <Post
-              key={index}
-              post_desc={post.post_description}
-              postID={post._id}
-              username={post.creator.name}
-              timeposted={post.time_posted}
-              userImg={post.creator.profilePicture}
-              photos={post.photos}
-            />
-          })
-        } 
+        <InfiniteScroll
+          dataLength={posts.length}
+          next={() => setPage(prev => prev + 1)}
+          hasMore={true} // Set to false when there's no more data to load
+          loader={<h4>Loading...</h4>} // Custom loading component
+          endMessage={<p>No more items to load</p>} // Message when there's no more data
+        >
+          {
+            posts.map((post, index) => {
+
+              return <div className={PostStyles.post__post} key={index}>
+                {/* Header */}
+                <div className={PostStyles.post__post_header}>
+                  <img src={post.creator.profilePicture == undefined ? " " : post.creator.profilePicture} className={PostStyles.post__user_pixs} alt={post.creator.name} />
+                  <div className={PostStyles.post__post_info}>
+                    <p className={PostStyles.post__user_name}>{post.creator.name}</p>
+                    <p>{dayjs(post?.time_posted).fromNow()}</p>
+                  </div>
+                </div>
+                {/* Description */}
+                <span>
+                  <p id={post?._id}>
+                    {post?.post_description}
+                  </p>
+                  <p className={PostStyles.post__seeMoreBtn}>See more</p>
+                </span>
+
+                {/* Photos */}
+                <div className={PostStyles.post__post_photos}>
+                  {
+                    post.photos.map((photo, index) => {
+                      return <div className={PostStyles.post__post_photoContainer} key={index}>
+                        <img src={photo} alt={`${post.creator.name} photo`} />
+                      </div>
+                    })
+                  }
+
+                </div>
+              </div>
+            })
+          }
+        </InfiniteScroll>
       </div>
 
 
