4e6ac258af41ef20878a5500ab855409177df811
joshua Oyewole <joshpp2013@gmail.com>
2023-07-18 20:53:17 +0100

    added profilePicture to useData Hook

--- client/src/util/auth.ts
+++ client/src/util/auth.ts
@@ -2,13 +2,14 @@
 export function getToken(): string | null {
     return localStorage.getItem('token');
 }
-export function getUserDatas(): {firstname:string| null, lastname:string| null, token:string| null, user_id:string| null}  {
+export function getUserDatas(): {firstname:string| null, lastname:string| null, token:string| null, user_id:string| null, profilePicture:string | null}  {
    const firstname =  localStorage.getItem('firstname');
    const lastname =  localStorage.getItem('lastname');
    const user_id =  localStorage.getItem('user_id');
    const token =  localStorage.getItem('token');
+   const profilePicture =  localStorage.getItem('profilePicture');
 
-   return {firstname,lastname,user_id,token}
+   return {firstname,lastname,user_id,token, profilePicture }
 }
 
 export function isAuthenticated(): boolean {
