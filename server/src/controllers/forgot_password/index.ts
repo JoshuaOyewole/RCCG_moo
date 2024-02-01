d7a7d9566068c25c81fdcff2dbcf0b91732a0b3f
joshua Oyewole <joshpp2013@gmail.com>
2024-02-01 15:14:19 +0100

    added layer for name

--- server/src/controllers/forgot_password/index.ts
+++ server/src/controllers/forgot_password/index.ts
@@ -8,7 +8,6 @@ import { hashData } from "../../util/hashData";
 const forgetPWD = async (req: Request, res: Response, next: NextFunction) => {
   try {
     let { email, name } = req.body;
-    console.log({ email, name });
 
     //trim off whiteSpace
     email = email.trim();
@@ -24,11 +23,113 @@ const forgetPWD = async (req: Request, res: Response, next: NextFunction) => {
       email,
       /* subject: 'Password Reset', */
       subject: '🚀 Congratulations! Your Next Steps Await!',
-      message: "Enter the Code below to reset your Password",
+      //message: "Enter the Code below to reset your Password",
+      message: `
+      <!DOCTYPE html>
+            <html lang="en">
+              <head>
+                <meta charset="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link
+                  href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+                  rel="stylesheet"
+                />
+                <title>Email</title>
+                <style>
+                  body {
+                  
+                    font-family: "Poppins", sans-serif;
+                    font-weight: 400;
+                    font-style: normal;
+                    font-size:16px;
+                  }
+                  .center {
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    text-align:center
+                  }
+                  .content {
+                    padding: 2rem 1rem;
+                    width:60vw;
+                    background-color: #f4f4f4;
+                  }
+                  .bg_color {
+                    background-color: #3e2b55;
+                    height: 5px;
+                    border-top-right-radius: 0.5rem;
+                    border-top-left-radius: 0.5rem;
+                    margin-top: 1rem;
+                  }
+                  p,h3 {
+                    line-height: 2rem;
+                    font-size:20px;
+                  }
+                  li{
+                    font-size:20px;
+                  }
+                  .small_height{
+                    line-height: 1.9rem;
+                  }
+                </style>
+              </head>
+              <body>
+                <div class="container" style="display: flex; justify-content: center;">
+                  <div class="bg_color"></div>
+                  <div class="content">
+                    <h2 class="header">Dear ${name},</h2>
+                    <p>
+                      We're thrilled that you've expressed interest in our Web Development
+                      Course, and we can't wait to embark on this coding journey with you!
+                    </p>
+                    <h3>🚀 Web Development Course Overview:</h3>
+                    <p>
+                      Our comprehensive course is designed to equip you with in-demand
+                      skills and kickstart your journey in web development. From building
+                      responsive websites to mastering coding languages, we cover it all!
+                    </p>
+                    <p>Your first class kick start shortly,</p>
+                    <h3>📅 First Session Details:</h3>
+                    <ul>
+                      <li>Date: Thursday, 1<sup>st</sup> February 2024</li>
+                      <li>Time: 7:00 pm</li>
+                    </ul>
+                    <h3>🌟 Why Join Us?</h3>
+                    <ul>
+                      <li>Exliert-led sessions</li>
+                      <li>Practical hands-on projects</li>
+                      <li>Interactive learning environment</li>
+                      <li>
+                        Networking opportunities with our partners after your training
+                      </li>
+                    </ul>
+                    <h3>📧 Next Steps:</h3>
+                    <p>
+                      Look out for an email with further details on the curriculum,
+                      materials, and how to prepare for the first session.
+                    </p>
+                    <p>
+                      We're excited to have you on board! If you have any questions or need
+                      assistance, feel free to reply to this email.
+                    </p>
+                    <p><b> Best regards,</b></p>
+                    <p class="small_height">
+                      Joshua Oyewole <br />
+                      <span style="font-style: italic;">Lead Tutor</span> <br />
+                      Orisfina Bootcamp <br />
+                      Hotline: 07032054367 <br />
+                      Website: <a href="https://oci.com.ng">oci.com.ng</a>
+                    </p>
+                  </div>
+                </div>
+              </body>
+            </html>
+      `,
       duration: 1,
       name
     }
-    console.log(otpDetails);
 
     await sendOTP(otpDetails);
     /* 
